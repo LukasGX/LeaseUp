1a0c68bcdbed2a7e46c5435535c9b5b2c891ae33
LukasGrambs <lukas@grambs.info>
2025-07-01 17:05:51 +0200

    Design

--- script.js
+++ script.js
@@ -1,12 +1,3 @@
 function request(offer) {
     window.location.href = `request.html?offer=${encodeURIComponent(offer)}`;
 }
-async function sidebar() {
-    const sidebar = document.getElementById('sidebar');
-    const sidebarPossibilities = ['survey', 'feedback', 'newsletter', 'contact'];
-    const randomSidebar = sidebarPossibilities[Math.floor(Math.random() * sidebarPossibilities.length)];
-
-    const response = await fetch(`sidebar/${randomSidebar}.html`);
-    sidebar.innerHTML = await response.text();
-}
-sidebar();
